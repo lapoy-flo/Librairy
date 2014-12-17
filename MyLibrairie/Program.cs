c4c7c71421eebe0a7dda591dfa0a3c8ff1fae9f3
Benjamin CORET <lemou65@gmail.com>
2014-12-17 12:00:12 +0100

    New modif, test double modif

--- MyLibrairie/Program.cs
+++ MyLibrairie/Program.cs
@@ -11,6 +11,7 @@ namespace MyLibrairie
         static void Main(string[] args)
         {
             Console.WriteLine("Salut");
+            Console.WriteLine("Nouvelle modification");
         }
     }
 }
